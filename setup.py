d0a673cfb3530f887f3c9612e802e97bc4b65f7f
kez <clealk@cardiff.ac.uk>
2020-03-17 10:48:46 +0000

    Readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="svbench",
-    version='0.1.0',
+    version='0.2.0',
     python_requires='>=3.7',
     install_requires=[
             'click',
