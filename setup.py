799e7660579be379835d21bede01220b59d99c5a
kcleal <clealk@cardiff.ac.uk>
2022-04-14 11:07:51 +0100

    Changed requirements from pyvcf -> pyvcf3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="svbench",
-    version='0.5.5',
+    version='0.6.0',
     license='MIT',
     python_requires='>=3.7',
     install_requires=[
@@ -10,7 +10,7 @@ setup(
             'pandas',
             'networkx>=2.4',
             'ncls',
-            'pyvcf',
+            'pyvcf3',
             'matplotlib'
         ],
     packages=find_packages(where="."),
