dbb96f5d38ab46494b302ba0b6da4512d2fe2ab5
kcleal <clealk@cardiff.ac.uk>
2023-07-06 16:14:05 +0100

    Added CLI function for basic benchmarking

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="svbench",
-    version='0.7.1',
+    version='0.7.2',
     license='MIT',
     python_requires='>=3.7',
     install_requires=[
