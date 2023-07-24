358d6d3442fe477f2c26f0e6206c9570e4aa7147
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-07-24 13:33:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="svbench",
-    version='0.7.4',
+    version='0.7.5',
     license='MIT',
     python_requires='>=3.7',
     install_requires=[
